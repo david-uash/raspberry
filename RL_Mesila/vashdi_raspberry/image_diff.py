7b4c0f2bf876e3811cc36b3f08ec6734e8692ff9
david vashdi <daviduash@gmail.com>
2020-10-22 23:05:17 +0300

    add check value of pixel around circle that was found

--- RL_Mesila/vashdi_raspberry/image_diff.py
+++ RL_Mesila/vashdi_raspberry/image_diff.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3.7
 
+# 255 = white
+
 import cv2
 import numpy as np
 import pandas as pd
@@ -46,16 +48,17 @@ if circles1 is not None:
     for (x,y,r) in circles1:
         print("x,y,r - "+str(x)+","+str(y)+","+str(r))
         if(int(y) > 45 and int(y) < 75):
-            if(int(r) < 52): 
-                print("found y in boundry, value of y: ",str(y))
-                print("found r in boundry, value of r: ",str(r))
-                #add if to check most of the value of pixel in the area of the point x,y
-                x1 = int(x)
-        cv2.circle(im001rgb,(x,y),r,(0,255,0),4)
-        cv2.rectangle(im001rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
+            if(int(r) < 60): 
+                 sub_matrix = im001bw[y-15:y+15,x-15:x+15]
+                 print("avg of sub matrix is: ",sub_matrix.mean())
+                 if(sub_matrix.mean() > 200):
+                    x1,y1,r1 = x,y,r
+                    print("average is above 200, x,y,z:",str(x1),str(y1),str(r1))
+                    cv2.circle(im001rgb,(x,y),r,(0,255,0),4)
+                    cv2.rectangle(im001rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
 else:
     print("no circle found")
-
+print("##############################################")
 
 circles2 = cv2.HoughCircles(im002bw,cv2.HOUGH_GRADIENT, dp=3.95,minDist=25,minRadius=25,maxRadius=70)
 if circles2 is not None:
@@ -64,34 +67,31 @@ if circles2 is not None:
     for (x,y,r) in circles2:
         print("x,y,r - "+str(x)+","+str(y)+","+str(r))
         if(int(y) > 45 and int(y) < 75):
-            if(int(r) < 52):
-                print("found y in boundry with currect r, value of y: ",str(y),"value of r: ",str(r))
-                #add if to check most of the value of pixel in the area of the point x,y
-                x2 = int(x)
-        cv2.circle(im002rgb,(x,y),r,(0,255,0),4)
-        cv2.rectangle(im002rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
+            if(int(r) < 60):
+                sub_matrix = im002bw[y-15:y+15,x-15:x+15]
+                print("avg of sub matrix is: ",sub_matrix.mean())
+                if(sub_matrix.mean() > 200):
+                    x2,y2,r2 = x,y,r
+                    print("average is above 200, x,y,z:",str(x2),str(y2),str(r2))
+                    cv2.circle(im002rgb,(x,y),r,(0,255,0),4)
+                    cv2.rectangle(im002rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
 else:
     print("no circle found")
 try:
-    if(x2 is not None and x1 is not None):
+    if(x1 is not None and x2 is not None):
+        circleDelta = np.zeros(im001bw.shape)
         deltaX = int(x2-x1)
         print("### delta x2-x1 = ",str(deltaX)," ###")
         print("normal value of delta x (x/640): ",float(deltaX/640))
+        circleDelta[y2-r2:y2+r2,x2-r2:x2+r2] = im002bw[y2-r2:y2+r2,x2-r2:x2+r2] - im001bw[y2-r2:y2+r2,x2-r2:x2+r2]
+    else:
+        circleDelta = np.zeros(im001bw.shape)
 except NameError:
     print("x1 or x2 were not found")
-circleDelta = np.zeros(im001bw.shape)
-if((circles1 is not None) and (circles2 is not None)):
-
-  circleDelta = np.zeros(im001bw.shape)
-  for x,y,r in np.concatenate((circles1,circles2)):
-      print("concatenate")
-      print("x,y,r - "+str(x)+","+str(y)+","+str(r))
-      circleDelta[y-r:y+r,x-r:x+r] = im002bw[y-r:y+r,x-r:x+r] - im001bw[y-r:y+r,x-r:x+r]
 
 print("my program took ",time.time() - start_time," to run")
 
 fig = plt.figure(figsize=(30,30))
-#figure,ax  = plt.subplots(3,2)
 
 plt1 = fig.add_subplot(4,2,1)
 plt.imshow(im001rgb)

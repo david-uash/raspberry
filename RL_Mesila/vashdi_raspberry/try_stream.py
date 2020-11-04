6c2bcadb900b5af5f84988796b02d74c4e000ebc
david vashdi <daviduash@gmail.com>
2020-11-04 22:27:18 +0200

    add live stream to run_game.py

--- RL_Mesila/vashdi_raspberry/try_stream.py
+++ RL_Mesila/vashdi_raspberry/try_stream.py
@@ -10,15 +10,15 @@ from random import randint
 import time
 import math
 
-import keras
-from keras.models import Sequential
-from keras.layers import Convolution2D 
-from keras.layers import Conv2D
-from keras.layers import MaxPooling2D
-from keras.layers import Flatten
-from keras.layers import Dense
+#import keras
+#from keras.models import Sequential
+#from keras.layers import Convolution2D 
+#from keras.layers import Conv2D
+#from keras.layers import MaxPooling2D
+#from keras.layers import Flatten
+#from keras.layers import Dense
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 
 
@@ -28,15 +28,6 @@ import RPi.GPIO as GPIO
 ### INIT ###
 ############
 
-
-### INIT SERVO ###
-#servoPIN = 17 #empty one for test
-servoPIN = 26 #game servo
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-print("setting servo to 7.5 [90deg]")
-p.start(7.5) # Initialization
 
 ### INIT CAMERA ###
 ###################################
@@ -52,42 +43,13 @@ for i in range(3,0,-1):
   print("take picture in: :"+str(i))
   time.sleep(1)
 
-### INIT CNN ###
-vector = np.array((0,0,0))
-###########
-### CNN ###
-###########
-inputshape = vector.shape
-model2 = Sequential()
-model2.add(Dense(12,input_dim=3,activation='relu'))
-model2.add(Dense(8,activation='relu'))
-model2.add(Dense(6,activation='relu'))
-model2.add(Dense(units=1,activation='sigmoid'))
-#model2.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-#model2.compile(loss='CategoricalCrossentropy',optimizer='adam',metrics=['accuracy'])
-model2.compile(loss='mean_squared_error',optimizer='adam',metrics=['mse'])
-print("the prediction for vector is: ",model2.predict(vector.reshape(1,3)))
-print("the prediction for vector is: ",model2.predict(np.zeros(3).reshape(1,3)))
-#model2.predict(np.zeros((1,3)))
-############################################
+while True:
+    _,pic = cap.read()
+    cv2.imshow("image",pic)
+    time.sleep(1)
+    cv2.waitKey(1)
 
-
-
-
-###########
-
-
-
-### INIT GPIO ###
-redpin = 22
-greenpin = 27
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(greenpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-#print("green: ",GPIO.input(greenpin))
-GPIO.setup(redpin,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-#print("red: ",GPIO.input(redpin))
-
-
+    
 
 ### GAME VARS ###
 sucess_time = 10
@@ -128,8 +90,8 @@ try:
                                 x1,y1,r1 = x,y,r
                                 circle1_set = True
                                 print("debug: average is above 200, x,y,z:",str(x1),str(y1),str(r1))
-                                #cv2.circle(im001rgb,(x,y),r,(0,255,0),4)
-                                #cv2.rectangle(im001rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
+                                cv2.circle(im001rgb,(x,y),r,(0,255,0),4)
+                                cv2.rectangle(im001rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
             else:
                 #print("debug: no circle found set x,y,r = 400,50,50")
                 x1,y1,r1 = 400,50,50
@@ -148,18 +110,11 @@ try:
         im002gray  = cv2.cvtColor(im002,cv2.COLOR_BGR2GRAY)
         (thresh,correntImageBW) = cv2.threshold(im002gray,180,255,cv2.THRESH_BINARY)
         im002bw = correntImageBW 
-        im002rgb = cv2.cvtColor(im002,cv2.COLOR_BGR2RGB)
-        cv2.imshow("im002rgb",im002rgb)
-        cv2.waitKey(1)
         circles2 = cv2.HoughCircles(im002bw,cv2.HOUGH_GRADIENT, dp=3.95,minDist=25,minRadius=25,maxRadius=70)
         if circles2 is not None:
             #print("debug: found circle in im002bw")
             circles2 = np.round(circles2[0,:]).astype("int")
             for (x,y,r) in circles2:
-                cv2.circle(im002rgb,(x,y),r,(0,255,0),4)
-                cv2.rectangle(im002rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
-                cv2.imshow("im002rgb",im002rgb)
-                cv2.waitKey(1)
                 #print("debug: x,y,r - "+str(x)+","+str(y)+","+str(r))
                 if(int(y) > 45 and int(y) < 75):
                     if(int(r) < 60):
@@ -171,8 +126,6 @@ try:
                             print("debug: average is above 200, x,y,z:",str(x2),str(y2),str(r2))
                             cv2.circle(im002rgb,(x,y),r,(0,255,0),4)
                             cv2.rectangle(im002rgb,(x-5,y-5),(x+5,y+5),(0,128,255),-1)
-                            cv2.imshow("im002rgb",im002rgb)
-                            cv2.waitKey(1)
         else:
             #print("debug: no circle found set x,y,r same as before (x1,y1,r1)")
             x2,y2,r2 = x1,y1,r1
@@ -188,81 +141,7 @@ try:
         #print("debug: normal value of delta x (x/640): ",float(deltaX/640))
         #circleDelta[y2-r2:y2+r2,x2-r2:x2+r2] = im002bw[y2-r2:y2+r2,x2-r2:x2+r2] - im001bw[y2-r2:y2+r2,x2-r2:x2+r2]
         
-        vector = np.array((deltaX/640,x2/640,predict_value_to_servo/7.5+0.5))
-        predict = model2.predict(vector.reshape(1,3))
-        predict_value_to_servo = ((predict[0][0] - 0.5)*2)*3.7
-            
-        x_train.append(vector)
-        y_train.append(predict)
-
-        x1,y1,r1 = x2,y2,r2
-
-
-
-        print("debug: predict deltabw: ",predict)
-        if(random() < math.exp(-i/200)):
-            randomnumber = randint(1,20)
-            predict_value_to_servo = (randomnumber-10)/2.7
-            print("debug: choosing random number:",predict_value_to_servo/7.5+0.5)
-        if(i%10 == 0):
-            print("predict_value_to_servo: ",predict_value_to_servo)
-        p.ChangeDutyCycle(7.5 + float(predict_value_to_servo))
-        currentImageBW = previousImageBW
-        if((GPIO.input(redpin) == 0) or (GPIO.input(greenpin) == 0)):
-            print("num of steps in this round : ",cRoundCounter)
-            lossesCounter += 1 
-            #print("bring ball back to middle")
-            if(cRoundCounter < 3):
-                for j in range(1,len(y_train),1): 
-                    y_train[len(y_train)-j] *= (-1)
-            elif(cRoundCounter < 10):
-                for j in range(1,2,1): #start at 0 until 3 in steps of 1 (0,1,2) 
-                    y_train[len(y_train)-j] *= (-1)
-            else:
-                for j in range(1,4,1):
-                    y_train[len(y_train)-j] *= (-1)
-
-            print("adjasting model")
-            start_time = time.time()
-            model2.fit(x=np.vstack(x_train),y=np.vstack(y_train))
-            #print("adjasting model took: ",time.time() - start_time)
-            x_train,y_train = [],[]
-            time.sleep(2)
-            dc = 7.5 + float(predict_value_to_servo)
-            if((GPIO.input(redpin) == 0)):
-                print("ball at red gate")
-                while((GPIO.input(redpin) == 0)):
-                    time.sleep(0.35)
-                    dc = dc + 0.1
-                    p.ChangeDutyCycle(dc)
-                time.sleep(0.35)
-                p.ChangeDutyCycle(7.7)
-                #time.sleep(0.1)
-                #p.ChangeDutyCycle(4.5)
-                #time.sleep(1.8)
-                #p.ChangeDutyCycle(9)
-                #time.sleep(0.77)
-                #p.ChangeDutyCycle(6.5)
-            else:
-                print("ball at green gate")
-                while((GPIO.input(greenpin) == 0)):
-                    time.sleep(0.35)
-                    dc = dc - 0.1
-                    p.ChangeDutyCycle(dc)
-                p.ChangeDutyCycle(7.85)
-                #time.sleep(0.1)
-                #p.ChangeDutyCycle(9.5)
-                #time.sleep(1.8)
-                #p.ChangeDutyCycle(4.5)
-                #time.sleep(0.67)
-                #p.ChangeDutyCycle(8.3)
-            time.sleep(0.15)
-            cRoundCounter = 0
-            gamenumber = gamenumber + 1
-            print("debug: starting to play again, game number:",gamenumber)            
-            #PSILA
-        ### to add check if one of the light switches has been pressed 
-
+#        image.show
 
 
 
